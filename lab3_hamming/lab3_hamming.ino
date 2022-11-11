7319810f78dc9723acc90139937a086c93b3978e
david <shmailov.david@gmail.com>
2022-11-11 13:00:01 +0200

    fixed bugs

--- lab3_hamming/lab3_hamming.ino
+++ lab3_hamming/lab3_hamming.ino
@@ -4,7 +4,7 @@
 #define RX_PIN 3
 #define CLK_OUT_PIN 4
 #define CLK_IN_PIN 5
-#define L2_BUFFER_SIZE 16
+#define L2_BUFFER_SIZE 13
 #define L2_COUNT_LIMIT L2_BUFFER_SIZE<<1
 #define P1_bit 6
 #define P2_bit 5
@@ -14,19 +14,21 @@
 #define D3_bit 1
 #define D4_bit 0
 
+#define with_error 0
 
 
 // usart_tx global variables
-const int BIT_TIME = 20;
-const int half_BIT_TIME = BIT_TIME >> 1;
-const int wait_max = 100*BIT_TIME;
-const int buffer_size = 8;
+#define BIT_TIME          20
+#define half_BIT_TIME     BIT_TIME >> 1
+#define wait_max          100*BIT_TIME
+#define wait_min          10*BIT_TIME
+#define buffer_size       8
 
 typedef enum {ACTIVE, PASSIVE} state_type;
 unsigned long start_time = 0;
 unsigned long curr_time = 0;
 unsigned long delta_t;
-int l1_tx_buffer = 0b01010101;
+int l1_tx_buffer;
 int l1_tx_count = 0;
 long l1_tx_wait = 0;
 int l1_tx_bit;
@@ -47,7 +49,7 @@ typedef enum {FIRST, SECOND} l2_state_type;
 l2_state_type half_state = FIRST;
 int layer2_tx_buffer_counter = 0;
 char l2_tx_buff [L2_BUFFER_SIZE] = "DAVID_NERIYA";
-int tx_hamming_counter =0;
+int tx_half_byte_lsb =1;
 int tx_byte_hamming = 0;
 int P1= 0;
 int P2= 0;
@@ -78,22 +80,23 @@ void usart_tx(){
           l1_tx_count ++;
           start_time = millis();
           if (l1_tx_count == buffer_size){
-            l1_tx_wait = random(wait_max);
+            l1_tx_wait = random(wait_min , wait_max);
             l1_tx_state = PASSIVE;
             l1_tx_count = 0;
-            layer_1_tx_busy = 0;
           }
         } else if (delta_t >= half_BIT_TIME){
           digitalWrite(CLK_OUT_PIN,LOW);
         }
         break;
     case PASSIVE:
-      if (delta_t > l1_tx_wait && layer_2_tx_request == 1){
-        l1_tx_state = ACTIVE;
-        start_time = millis();
-        layer_2_tx_request = 0;
-        layer_1_tx_busy = 1;
-      }
+      if (delta_t > l1_tx_wait)
+        layer_1_tx_busy = 0;
+        if(  layer_2_tx_request == 1){
+          l1_tx_state = ACTIVE;
+          start_time = millis();
+          layer_2_tx_request = 0;
+          layer_1_tx_busy = 1;
+        }
       break;
   }
 }
@@ -105,32 +108,31 @@ void usart_rx(){
     l1_rx_bit = digitalRead(RX_PIN);
     bitWrite(l1_rx_buffer, l1_rx_count, l1_rx_bit);
     l1_rx_count ++;
-    clk_in_prev = clk_in_curr;
     if (l1_rx_count == buffer_size){
       l1_rx_count = 0;
       layer_1_rx_busy = 0;
     }
-  }else{
-    clk_in_prev = clk_in_curr;
   }
+  clk_in_prev = clk_in_curr;
+  
 }
   
 
 void link_layer_tx(){
-  if (layer_1_tx_busy == 0){
+  if (!layer_1_tx_busy && !layer_2_tx_request){
     if (layer2_tx_buffer_counter<=L2_BUFFER_SIZE){
-      layer_2_tx_request=1;
-      if (tx_hamming_counter==0){
+      if (tx_half_byte_lsb){
         tx_byte_hamming = l2_tx_buff[layer2_tx_buffer_counter];
         l1_tx_buffer = Hamming47_tx();
-        tx_hamming_counter=1;
+        tx_half_byte_lsb = 0;
       }
       else{
         tx_byte_hamming = l2_tx_buff[layer2_tx_buffer_counter]>>4;
         l1_tx_buffer = Hamming47_tx();
-        tx_hamming_counter = 0;
+        tx_half_byte_lsb = 1;
         layer2_tx_buffer_counter++;
       }
+      layer_2_tx_request=1;
     }
   }
 }
@@ -138,22 +140,23 @@ void link_layer_tx(){
 void link_layer_rx(){
 
   if (layer_1_rx_busy < layer_1_rx_busy_prev){ // falling edge of rx_busy flag
+    layer_1_rx_busy = 1; // just making sure to not doudle read
     if (l2_rx_counter % 2 == 0){ // even number of bits
       half_byte_lsb = Hamming47_rx();
     }else{ // odd number of bits
       half_byte_msb = Hamming47_rx();
       l2_buffer_pos = l2_rx_counter >> 1;
       l2_rx_buff[l2_buffer_pos] = (half_byte_msb << 4) | half_byte_lsb;
-      // Serial.print("Received char: ");
-      // Serial.print(l2_rx_buff[l2_buffer_pos]);
-      // Serial.print("\n");
+      Serial.print("Received char: ");
+      Serial.print(l2_rx_buff[l2_buffer_pos]);
+      Serial.print("\n");
     }
     l2_rx_counter ++;
     if (l2_rx_counter == L2_COUNT_LIMIT){
       l2_rx_counter = 0;
-      // Serial.print("Received String: ", l2_rx_buff);
-      // Serial.print(l2_rx_buff);
-      // Serial.print("\n");
+      Serial.print("Received String: ");
+      Serial.print(l2_rx_buff);
+      Serial.print("\n");
     }
   }
   layer_1_rx_busy_prev = layer_1_rx_busy; 
@@ -171,17 +174,21 @@ char Hamming47_tx(){
       bitWrite(temp_l2,D2_bit,bitRead(tx_byte_hamming,2));
       bitWrite(temp_l2,D3_bit,bitRead(tx_byte_hamming,1));
       bitWrite(temp_l2,D4_bit,bitRead(tx_byte_hamming,0));
-      // Serial.print("Sending byte: ", temp_l2);
-      // Serial.print(temp_l2,BIN);
-      // Serial.print("\n");
+      Serial.print("Sending  byte: ");
+      Serial.print(temp_l2,BIN);
+      Serial.print("\n");
+      if (with_error){
+        int bit_to_flip = random(0,6);
+        bitWrite(temp_l2, bit_to_flip ,!bitRead(temp_l2,bit_to_flip));
+      }
       return temp_l2;
 }
 
 char Hamming47_rx(){
   char temp_rx=l1_rx_buffer;
-  // Serial.print("Recieved byte: ");
-  // Serial.print(temp_rx,BIN);
-  // Serial.print("\n");
+  Serial.print("Received byte: ");
+  Serial.print(temp_rx,BIN);
+  Serial.print("\n");
   S1 = bitRead(temp_rx, P1_bit) ^ bitRead(temp_rx, D1_bit) ^ bitRead(temp_rx, D2_bit) ^ bitRead(temp_rx, D4_bit);
   S2 = bitRead(temp_rx, P2_bit) ^ bitRead(temp_rx, D1_bit) ^ bitRead(temp_rx, D3_bit) ^ bitRead(temp_rx, D4_bit);
   S3 = bitRead(temp_rx, P3_bit) ^ bitRead(temp_rx, D2_bit) ^ bitRead(temp_rx, D3_bit) ^ bitRead(temp_rx, D4_bit);
@@ -189,9 +196,9 @@ char Hamming47_rx(){
   if (S>0){
     int index = 7-(S);
     temp_rx = temp_rx ^ (1 << index);
-    // Serial.print("Fixed byte: ");
-    // Serial.print(temp_rx,BIN);
-    // Serial.print("\n");
+    Serial.print("Fixed byte: ");
+    Serial.print(temp_rx,BIN);
+    Serial.print("\n");
   }
   char data = (bitRead(temp_rx, D1_bit) << 3) | (bitRead(temp_rx, D2_bit) << 2) | (bitRead(temp_rx, D3_bit) << 1) | bitRead(temp_rx, D4_bit);
   return data;
